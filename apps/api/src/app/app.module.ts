2bdebfb2d2f4da7514ad11c976dca11ef4ba7529
James Allen <james.allen@meta.ceo>
2020-08-08 22:27:56 -0500

    feat: serving static site during production

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
+import { environment } from '../environments/environment';
 import { AppController } from './app.controller';
 
+const productionModules = environment.production
+  ? [ ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'nx-demo-swagger-decorators-app'),
+    })]
+  : [];
+
 @Module({
+  imports: [
+    ...productionModules
+  ],
   controllers: [AppController]
 })
 export class AppModule {}
