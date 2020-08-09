715199b2b4b574b879756415f2fdf9fe4bc8797f
James Allen <james.allen@meta.ceo>
2020-08-08 21:47:27 -0500

    feat: adding swagger work

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -1,8 +1,14 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { MessageRequest, MessageResponse } from '@nx-demo-swagger-decorators/api-interfaces';
+import { ApiResponse } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
+
+  @ApiResponse({
+    description: 'Return the provided message array with a timestamp.',
+    type: MessageResponse
+  })
   @Post('hello')
   getData(@Body() body: MessageRequest): MessageResponse {
     const { message } = body;
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,10 +1,6 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module';
 
@@ -12,6 +8,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+
+  const options = new DocumentBuilder()
+    .setTitle('nx-demo-swagger-decorators')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   const port = process.env.PORT || 3333;
   await app.listen(port, () => {
     Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
